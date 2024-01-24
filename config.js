ea40fc0262b71bb311a204f0b9593cc89339e289
H3rcu1es <68457852+H3rcu1es@users.noreply.github.com>
2024-01-24 17:25:30 +0600

    testing

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const CONFIG = {
 
 	// Greetings
 	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,salksa',
+	greetingAfternoon: 'Good afterMoon' ,
 	greetingEvening: 'Good evening,',
 	greetingNight: 'Go to sleep ',
 
