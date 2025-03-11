2849ccc9ca27480817f78133fcb2038a3f90c104
H3rcu1es <kayfee529@gmail.com>
2025-03-11 21:37:34 +0600

    layout change

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'Go to sleep ',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: 'c16080f7df3882535ae7c2dda37556fc', // Write here your API Key
